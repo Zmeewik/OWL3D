841cde78d1f29a5814b27ae4a83dc0d6664cd08a
Doom_Enjoyer <zmeelovme@gmail.com>
2025-04-10 17:28:18 +0300

    BugFix propeller

--- Assets/Scripts/Mechanisms/Propeller.cs
+++ Assets/Scripts/Mechanisms/Propeller.cs
@@ -13,7 +13,7 @@ public class Propeller : MonoBehaviour, IInteractable
     public void Activate(Collider goal)
     {
         var goalRb = goal.GetComponent<Rigidbody>();
-        goalRb.velocity = new Vector3(goalRb.velocity.x, 0f, goalRb.velocity.y);
+        goalRb.velocity = new Vector3(goalRb.velocity.x, 0f, goalRb.velocity.z);
         goalRb.AddForce(Vector3.up * tossForce, ForceMode.Impulse);
     }
 
