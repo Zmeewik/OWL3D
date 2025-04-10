71481c0d588a65eca14f8c7733026bf081adea31
Doom_Enjoyer <zmeelovme@gmail.com>
2025-04-10 17:27:19 +0300

    Added propeller
    Added propeller that lifts player up in trigger

--- Assets/Scripts/Mechanisms/Propeller.cs
+++ Assets/Scripts/Mechanisms/Propeller.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Mechanism))]
 public class Propeller : MonoBehaviour, IInteractable
 {
 
+    [SerializeField] float tossForce;
+    [SerializeField] Transform rotatingHelix;
+    [SerializeField] float rotationSpeed;
 
     public void Activate(Collider goal)
     {
-        var goalRb = other.GetComponent<Rigidbody>();
+        var goalRb = goal.GetComponent<Rigidbody>();
+        goalRb.velocity = new Vector3(goalRb.velocity.x, 0f, goalRb.velocity.y);
+        goalRb.AddForce(Vector3.up * tossForce, ForceMode.Impulse);
     }
 
 
+    void FixedUpdate()
+    {
+        rotatingHelix.Rotate(0,rotationSpeed,0);
+    }
 }
